3cc9577b7f234fd165b0e9df9f823757cc7e68b8
ehdxorhrl <leedj6477@naver.com>
2023-09-20 18:25:15 +0900

    y축 이동을 만듦
    y축 이동을 만들었다

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -7,27 +7,32 @@ open_canvas()
 # fill here
 grass=load_image('grass.png')
 character=load_image('character.png')
+
 x=0
 y=90
 
-def render_all(x,y):
+def render_all(_x,_y):
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(x, y)
+    character.draw_now(_x, _y)
     delay(0.02)
 
 
 def run_rectagle():
     print("rectagle")
 
+    global x
+    global y
+    
     #bottom->right
-    for x in range(50,750,10):
+    for x in range(50,750+10,10):
         render_all(x,y)
 
-    for x in range(750,50,-10):
+    for y in range(90,550+10,10):
         render_all(x,y)
-   
-    pass
+
+    for x in range(750,50-10,-10):
+        render_all(x,y)
 
 def run_circle():
     cx,cy,r=400,300,200
