78118172976f75f939af031bcf590ae6a391afb5
ehdxorhrl <leedj6477@naver.com>
2023-09-20 17:43:45 +0900

    rectagle, circle 뼈대 완성
    rectagle, circle 뼈대를 완성함

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -1,7 +1,27 @@
+import math
+
 from pico2d import *
 
 open_canvas()
 
 # fill here
+grass=load_image('grass.png')
+character=load_image('character.png')
+x=0
+y=90
+
+def run_rectagle():
+    print("rectagle")
+    pass
+
+def run_circle():
+    print("circle")
+    pass
+
+while (True):
+    run_rectagle()
+    run_circle()
+    break
+
 
 close_canvas()
